e1a8668db8f37b27e02b00c9aa90abdc27cedb72
grapefd <1373607710@qq.com>
2018-11-18 10:21:45 +0800

    添加一个新的路由，名为logout

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
 
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+
 
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
